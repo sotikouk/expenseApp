fb5315d2f98df372b0c9b367b205e935a8a16188
sotikouk <sotikou@uth.gr>
2023-05-04 20:39:18 +0300

    manage expense screen

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,9 +1,26 @@
 import { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { StyleSheet, View } from "react-native";
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/Styles";
+import Button from "../components/UI/Button";
 
 function ManageExpense({route, navigation}) {
     const editedExpenseId = route.params?.expenseId;
     const isEditing = !!editedExpenseId;
+
+    function deleteExpenseHandler() {
+        navigation.goBack();
+
+    }
+
+    function cancelHandler() {
+        navigation.goBack();
+    }
+
+    function confirmHandler() {
+        navigation.goBack();
+
+    }
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -11,7 +28,39 @@ function ManageExpense({route, navigation}) {
         });
     }, [navigation, isEditing]);
 
-    return (<Text>Manage expence screen!</Text>);
+    return (
+            <View style={styles.container}>
+                <View style={styles.buttons}>
+                    <Button style={styles.button} mode='flat' onPress={cancelHandler}>Cancel</Button>
+                    <Button style={styles.button} onPress={confirmHandler}>{isEditing ? 'Update' : 'Add'}</Button>
+                </View>
+                    <View style={styles.deleteContainer}>
+                        {isEditing && <IconButton icon={"trash"} color={GlobalStyles.colors.error500} size={36} onPress={deleteExpenseHandler}/>}
+                    </View>
+            </View>);
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+    },
+    deleteContainer: {
+        marginTop: 16,
+        paddingTop: 8,
+        borderTopWidth: 2,
+        borderTopColor: GlobalStyles.colors.primary200,
+        alignItems: 'center'
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'   
+    },
+    button: {
+        minWidth: 120,
+        marginHorizontal: 8
+    }
+});
